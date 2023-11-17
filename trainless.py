d3c2b4a378cdddae69a62971be2129542afd9822
mendez71 <89602851+mendez71@users.noreply.github.com>
2023-11-17 16:12:25 -0500

    Update trainless.py

--- trainless.py
+++ trainless.py
@@ -5,6 +5,7 @@ import argparse
 import importlib
 import os
 import subprocess
+import torch
 from v_diffusion import make_beta_schedule
 from moving_average import init_ema_model
 from torch.utils.tensorboard import SummaryWriter
@@ -17,7 +18,7 @@ def make_argument_parser():
     parser.add_argument("--dname", help="Distillation name. Data will be saved to ./checkpoints/<name>/<dname>/.", type=str, required=True)
     parser.add_argument("--checkpoint_to_continue", help="Path to checkpoint.", type=str, default="")
     parser.add_argument("--num_timesteps", help="Num diffusion steps.", type=int, default=1024)
-    parser.add_argument("--num_iters", help="Num iterations.", type=int, default=200000)  # Half of the previous value
+    parser.add_argument("--num_iters", help="Num iterations.", type=int, default=100000)  # Double the previous value
     parser.add_argument("--batch_size", help="Batch size.", type=int, default=1)
     parser.add_argument("--lr", help="Learning rate.", type=float, default=5e-5)
     parser.add_argument("--scheduler", help="Learning rate scheduler.", type=str, default="StrategyConstantLR")
@@ -26,22 +27,6 @@ def make_argument_parser():
     parser.add_argument("--ckpt_interval", help="Checkpoints saving interval in minutes.", type=int, default=30)
     parser.add_argument("--num_workers", type=int, default=-1)
     return parser
-
-def generate_images(module_name, checkpoint_path, output_path, batch_size=1):
-    """
-    Function to generate images using sample.py
-    :param module_name: Name of the model module
-    :param checkpoint_path: Path to the model checkpoint
-    :param output_path: Path to save the generated images
-    :param batch_size: Batch size for image generation
-    """
-    subprocess.run([
-        "python", "./sample.py",
-        "--module", module_name,
-        "--checkpoint", checkpoint_path,
-        "--out_file", output_path,
-        "--batch_size", str(batch_size)
-    ])
 
 def train_model(args, make_model, make_dataset):
     if args.num_workers == -1:
@@ -54,14 +39,6 @@ def train_model(args, make_model, make_dataset):
 
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
     test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
-
-    teacher_ema = make_model().to(device)
-
-    checkpoints_dir = os.path.join("checkpoints", args.name, args.dname)
-    if not os.path.exists(checkpoints_dir):
-        os.makedirs(checkpoints_dir)
-
-    scheduler = make_sheduler(args)
 
     teacher = make_model().to(device)
     teacher_ema = make_model().to(device)
@@ -79,7 +56,7 @@ def train_model(args, make_model, make_dataset):
     tensorboard = SummaryWriter(os.path.join(checkpoints_dir, "tensorboard"))
 
     teacher_diffusion = make_diffusion(teacher, args.num_timesteps, 1, device)
-    teacher_ema_diffusion = make_diffusion(teacher, args.num_timesteps, 1, device)
+    teacher_ema_diffusion = make_diffusion(teacher_ema, args.num_timesteps, 1, device)
 
     image_size = teacher.image_size
 
@@ -87,10 +64,14 @@ def train_model(args, make_model, make_dataset):
     diffusion_train = DiffusionTrain(scheduler)
     diffusion_train.train(train_loader, teacher_diffusion, teacher_ema, args.lr, device, make_extra_args=make_condition, on_iter=on_iter)
 
-    # Generate images after training
-    checkpoint_path = os.path.join(checkpoints_dir, "final_checkpoint.pt")  # Adjust this path as needed
-    output_path = os.path.join(checkpoints_dir, "generated_image.png")  # Adjust this path as needed
-    generate_images(args.module, checkpoint_path, output_path)
+    # Save the checkpoint at the end of training
+    checkpoint_path = os.path.join(checkpoints_dir, "final_checkpoint.pt")
+    torch.save({
+        "G": teacher.state_dict(),
+        "n_timesteps": args.num_timesteps,
+        "time_scale": 1  # Assuming time_scale is 1, adjust if different
+    }, checkpoint_path)
+    print("Checkpoint saved at:", checkpoint_path)
 
     print("Finished.")
 
