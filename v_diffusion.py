45e7627dd62cd40ecdfcba8797cae975b2eaad60
mendez71 <89602851+mendez71@users.noreply.github.com>
2023-11-20 20:27:11 -0500

    Update v_diffusion.py

--- v_diffusion.py
+++ v_diffusion.py
@@ -8,23 +8,19 @@ def make_diffusion(model, n_timestep, time_scale, device):
     betas = make_beta_schedule("cosine", cosine_s=8e-3, n_timestep=n_timestep).to(device)
     return GaussianDiffusion(model, betas, time_scale=time_scale)
 
-
-def make_beta_schedule(
-        schedule, n_timestep, linear_start=1e-4, linear_end=2e-2, cosine_s=8e-3
-):
+def make_beta_schedule(schedule, n_timestep, linear_start=1e-4, linear_end=2e-2, cosine_s=8e-3):
     if schedule == "cosine":
-        timesteps = (
-                torch.arange(n_timestep + 1, dtype=torch.float64) / n_timestep + cosine_s
-        )
+        timesteps = (torch.arange(n_timestep + 1, dtype=torch.float64) / n_timestep + cosine_s)
         alphas = timesteps / (1 + cosine_s) * math.pi / 2
         alphas = torch.cos(alphas).pow(2)
         alphas = alphas / alphas[0]
         betas = 1 - alphas[1:] / alphas[:-1]
         betas = betas.clamp(max=0.999)
+    elif schedule == "uniform":
+        betas = torch.linspace(linear_start, linear_end, n_timestep, dtype=torch.float64)
     else:
-        raise Exception()
+        raise ValueError("Invalid beta schedule")
     return betas
-
 
 def E_(input, t, shape):
     out = torch.gather(input, 0, t)
